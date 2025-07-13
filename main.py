038e33b978e32f26fe82d36df6b19cd10d31353c
Max-Pare <resilientcrab@proton.me>
2025-07-13 18:27:49 +0200

    begin work on webserver

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask('Grandma tracker')
-
+_COLOR_PALETTE = ('#3D8D7A', '#B3D8A8', '#FBFFE4', '#A3D1C6')
 @app.route('/')
 def hello_world():
     return render_template('index.html')
